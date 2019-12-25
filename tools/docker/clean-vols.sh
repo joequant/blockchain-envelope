4228fcf2045f5bd49d38e7bfce0a12358ec0a0a0
Joseph C Wang <joequant@gmail.com>
2019-12-25 19:35:14 +0800

    fix up clean-vols.sh

--- tools/docker/clean-vols.sh
+++ tools/docker/clean-vols.sh
@@ -3,4 +3,4 @@ set -e
 docker-compose stop $1 || echo
 ../tools/docker/rm-stopped-containers.sh || echo
 ../tools/docker/rm-untagged-images.sh || echo
-docker volume rm document_server_user || echo
+docker volume rm $2 || echo
