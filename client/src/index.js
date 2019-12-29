7954551b4a611ec2796155378ccf54328ce550cf
Joseph C Wang <joequant@gmail.com>
2019-12-29 19:17:07 +0800

    add client items

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,9 @@
 import { ApolloClient } from 'apollo-client';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { HttpLink } from 'apollo-link-http';
+import React from 'react';
+import { render } from 'react-dom';
+import { ApolloProvider } from '@apollo/react-hooks';
 
 const cache = new InMemoryCache();
 const link = new HttpLink({
@@ -11,3 +14,14 @@ const client = new ApolloClient({
   cache,
   link
 });
+
+
+const App = () => (
+  <ApolloProvider client={client}>
+    <div>
+      <h2>My first Apollo app 🚀</h2>
+    </div>
+  </ApolloProvider>
+);
+
+render(<App />, document.getElementById('root'));
