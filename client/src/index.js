781ea8cb7e22af8291acecc858b3eb976f6b59d4
Joseph C Wang <joequant@gmail.com>
2019-12-29 20:31:54 +0800

    add tabs

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,11 @@
 import { ApolloClient } from 'apollo-client';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { HttpLink } from 'apollo-link-http';
+import React from 'react';
+import { render } from 'react-dom';
+import { ApolloProvider } from '@apollo/react-hooks';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
 
 const cache = new InMemoryCache();
 const link = new HttpLink({
@@ -11,3 +16,24 @@ const client = new ApolloClient({
   cache,
   link
 });
+
+
+const App = () => (
+<Tabs>
+  <TabList>
+     <Tab>Tab 1</Tab>
+     <Tab>Tab 2</Tab>
+  </TabList>
+  <TabPanel>
+     <ApolloProvider client={client}>
+     <div>
+        <h2>My first Apollo app 🚀</h2>
+     </div>
+     </ApolloProvider>
+  </TabPanel>
+  <TabPanel>
+  </TabPanel>
+</Tabs>
+);
+
+render(<App />, document.getElementById('root'));
