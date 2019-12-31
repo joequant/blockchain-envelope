b6e3a72382e3b865800e0b78993231532d54540a
Joseph C Wang <joequant@gmail.com>
2019-12-31 20:02:22 +0800

    add material-ui react

--- client/src/index.js
+++ client/src/index.js
@@ -4,8 +4,14 @@ import { HttpLink } from 'apollo-link-http';
 import React from 'react';
 import { render } from 'react-dom';
 import { ApolloProvider } from '@apollo/react-hooks';
-import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
-import 'react-tabs/style/react-tabs.css';
+
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import TabPanel from './tab-panel';
+
+
 
 const cache = new InMemoryCache();
 const link = new HttpLink({
@@ -18,22 +24,52 @@ const client = new ApolloClient({
 });
 
 
-const App = () => (
-<Tabs>
-  <TabList>
-     <Tab>Tab 1</Tab>
-     <Tab>Tab 2</Tab>
-  </TabList>
-  <TabPanel>
-     <ApolloProvider client={client}>
-     <div>
-        <h2>My first Apollo app 🚀</h2>
-     </div>
-     </ApolloProvider>
-  </TabPanel>
-  <TabPanel>
-  </TabPanel>
-</Tabs>
-);
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
 
-render(<App />, document.getElementById('root'));
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.paper,
+  },
+}));
+
+export default function SimpleTabs() {
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Tabs value={value} onChange={handleChange} aria-label="simple tabs example">
+          <Tab label="Apollo item" {...a11yProps(0)} />
+          <Tab label="Item Two" {...a11yProps(1)} />
+          <Tab label="Item Three" {...a11yProps(2)} />
+        </Tabs>
+       </AppBar>
+       <TabPanel value={value} index={0}>
+           <ApolloProvider client={client}>
+           <div>
+              <h2>My first Apollo app 🚀</h2>
+           </div>
+           </ApolloProvider>
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        Item Two
+      </TabPanel>
+      <TabPanel value={value} index={2}>
+        Item Three
+      </TabPanel>
+    </div>
+  );
+}
+
+render(<SimpleTabs />, document.getElementById('root'));
